9ae9727d8961219c256e4660b2e7d7b03e87ec8a
Matthew Hammond <ma>
2020-12-02 00:14:42 +0000

    Updated Image Src

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App flex-container">
 
       <div className="hero flex-box">
-        <img alt="Home Automation Systems" src="/Show_Villa_3D_Type_Pool.JPG"></img>
+        <img alt="Home Automation Systems" src="Show_Villa_3D_Type_Pool.JPG"></img>
       </div>
       <div className="content flex-box">
         <header className="App-header">
