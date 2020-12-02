e451aaf07c28645aaf2521a450b0ccb54cfee149
Matthew Hammond <ma>
2020-12-02 09:42:09 +0000

    Disrupting

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
       <div className="content flex-box">
         <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <p>Distrupting Home Automation Solutions</p>
+            <p>Disrupting Home Automation Solutions</p>
             <p>E: enquire@zling.com</p>
         </header>
         <Particles></Particles>
